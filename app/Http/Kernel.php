565f55a32a57ef568ce27df10366513ff4a93730
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-07-12 01:21:14 -0300

    [add] Auth Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'authenticateLogin' => \App\Http\Middleware\AuthenticateLogin::class,
     ];
 }
