fdc8105781283f8c6e320d95910e660adaaac0a4
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-07-13 01:20:02 -0300

    [add] @yield('content') and import app.css

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        @vite('resources/css/app.css')
         <title>{{ $title ?? 'NFinance' }}</title>
     </head>
     <body>
-        {{ $slot }}
+        @yield('content')
+        {{ $slot ?? '' }}
     </body>
 </html>
