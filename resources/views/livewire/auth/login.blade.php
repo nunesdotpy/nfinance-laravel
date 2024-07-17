165483ad58ccca0c2f9b2bbfb2ff8c02e3a6072d
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-07-17 00:21:12 -0300

    [feat] página de login

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,24 +1,38 @@
-<div class="flex justify-center items-center h-screen">
-    <div class="w-1/3">
-        <form wire:submit="login" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+<div class="d-flex login">
+    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+
+    <div class="align-content-center text-center aside col-md-8">
+        <h1 class="font-weight-bold">NFinance</h1>
+        <h2>Sua plataforma de gestão financeira</h2>
+    </div>
+    <div class="align-content-center m-auto">
+        
+        <form wire:submit="login">
             @csrf
-            <div class="mb-4">
-                <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
-                <input id="email" type="email" wire:model="email" value="{{ old('email') }}" required autofocus class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            <div>
+                <label for="email">Email</label>
+                <input id="email" type="email" wire:model="email" value="{{ old('email') }}" required autofocus>
             </div>
-            <div class="mb-6">
-                <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                <input id="password" type="password" wire:model="password" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            <div>
+                <label for="password">Password</label>
+                <input id="password" type="password" wire:model="password" required>
             </div>
-            <div class="flex items-center justify-between">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login</button>
+            <div class="text-end">
+                <a href="jaja">Forgot your password?</a>
+            </div>
+            @if(Session::has('error'))
+            <div class="alert alert-danger">
+                {{ Session::get('error') }}
+            </div>
+            @endif
+            <div>
+                <button type="submit">Login</button>
             </div>
         </form>
-        <div class="text-center">
-            <a href="{{ route('register') }}" class="text-blue-500 hover:text-blue-700">Register</a>
+        <div>
+            <a href="{{ route('register') }}">Register</a>
         </div>
-        {{-- <div class="text-center">
-            <a href="{{ route('password.request') }}" class="text-blue-500 hover:text-blue-700">Forgot your password?</a>
-        </div> --}}
+        
+        
     </div>
 </div>
