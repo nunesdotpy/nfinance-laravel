0ae30e648b1cc0dfcbdd9d4da33e92497f3332c5
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-07-17 00:20:20 -0300

    [feat] navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,29 @@
+
+<nav>
+    
+    <ul>
+        <li><a href="{{ route('home') }}">
+            <i class="bi bi-house"></i>
+            Home
+        </a></li>
+        <li><a href="/transactions">
+            <i class="bi bi-arrow-down-up"></i>
+            Transactions
+        </a></li>
+        <li>
+            <button>
+                <i class="bi bi-plus-circle-fill"></i>
+            </button>
+        </li>
+        <li><a href="/account">
+            <i class="bi bi-person-circle"></i>
+            Account
+        </a></li>
+        <li>
+            <a href="{{ route('logout') }}">
+                <i class="bi bi-box-arrow-right"></i>
+                Logout
+            </a>
+        </li>
+    </ul>
+</nav>
