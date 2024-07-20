8f3ba13f3de9e697d51817341e7360acfa9e6a23
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-07-20 00:30:46 -0300

    [feat] list user transaction component

--- resources/views/livewire/balance/user-transactions.blade.php
+++ resources/views/livewire/balance/user-transactions.blade.php
@@ -1,5 +1,34 @@
 <div>
-    @section('conteudo')
-    <h2>hello world</h2>
-    @endsection
+    @foreach ($transactions as $transaction)
+        @if (isset($hideTransaction[$transaction['_id']]['id']) &&
+                $hideTransaction[$transaction['_id']]['id'] == $transaction['_id']
+        )
+            {{-- @if (Session::has('deleted'))
+            @else
+            <div class="transaction deleted">
+                @endif --}}
+            <div class="transaction deleted">
+            @else
+                {{-- se não foi deletada --}}
+                <div class="transaction">
+        @endif
+
+        @if (Session::has('error'))
+            <div class="alert alert-danger">
+                {{ Session::get('error') }}
+            </div>
+        @endif
+
+        <div class="transaction-spec">
+            <p>{{ $transaction['name'] }}</p>
+            <p>R${{ $transaction['amount'] }}</p>
+            <p>{{ $transaction['description'] }}</p>
+            <p>{{ $transaction['date'] }}</p>
+        </div>
+        <div class="transaction-options">
+            <button class="btn-primary" wire:click="editTransaction('{{ $transaction['_id'] }}')">Edit</button>
+            <button class="btn-primary" onclick="deleteTransaction()" wire:click="delete('{{ $transaction['_id'] }}')">Delete</button>
+        </div>
+</div>
+@endforeach
 </div>
