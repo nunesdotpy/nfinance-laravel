fdfeae7aca129b9162cb8522b7f06b75a51c140c
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-07-20 00:29:05 -0300

    [feat] deleta transação

--- app/Livewire/Balance/UserTransactions.php
+++ app/Livewire/Balance/UserTransactions.php
@@ -9,12 +9,25 @@ class UserTransactions extends Component
 {
     public $user;
     public $transactions;
+    public $hideTransaction;
 
     public function mount()
     {
         $userid = $this->user['id'];
         $transactions = Controller::fetch(env("API_URL") . "spent/index/$userid", "GET", null, session("token"));
         $this->transactions = $transactions['data'];
+    }
+
+    public function delete($id)
+    {
+        $response = Controller::fetch(env("API_URL") . "transaction/delete/$id", "DELETE", null, session("token"));
+        if ($response['httpcode'] != 200) {
+            return back()->with('error', $response['message']);
+        }
+
+        $this->hideTransaction[$id]['id'] = $id;
+   
+        return back()->with('deleted');
     }
 
     public function render()
