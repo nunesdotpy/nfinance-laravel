77c2b377af2303c984c6efc1e8e50899b69fbc5c
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-07-25 00:55:15 -0300

    [feat] post new transaction

---
+++ app/Livewire/Balance/NewTransaction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire\Balance;
+
+use Livewire\Component;
+use App\Http\Controllers\Controller;
+
+class NewTransaction extends Component
+{
+
+    public $name;
+    public $amount;
+    public $type = "spent";
+    public $description;
+    public $date;
+    public $category;
+    public $userid;
+
+    public function newTransaction()
+    {
+        $user = session()->all();
+        $this->userid = $user['id'];
+        $request = $this->only(['name', 'amount', 'type', 'description', 'date', 'category', 'userid']);
+
+        $response = Controller::fetch(env("API_URL")."$this->type/register/$this->userid", "POST", $request, session("token"));
+
+        if($response['httpcode'] != 200){
+            return back()->with('error', $response['message']);
+        }
+
+        return redirect()->route('home')->with('success', $response['message']);
+    }
+
+    public function render()
+    {
+        return view('livewire.balance.new-transaction');
+    }
+}
